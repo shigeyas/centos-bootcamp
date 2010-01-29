b4815ecb2384bb942f13dcb14a3ccae60b5408ea
Shigeya Suzuki <shigeya@wide.ad.jp>
2010-01-29 14:05:16 +0900

    Minor bug fix for rpmforge-setup.sh

--- initial/rpmforge-setup.sh
+++ initial/rpmforge-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DIR=`dirname $0`
-rpm -Uvh $DIR/RPMS/rpmforge-release-0.3.6-1.el5.rf.i386.rpm
+rpm -Uvh $DIR/../RPMS/rpmforge-release-0.3.6-1.el5.rf.i386.rpm
 #sed -i 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/rpmforge.repo
