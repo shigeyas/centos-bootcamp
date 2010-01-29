ba9d2a66b4db476e023c1663efecfde20cfd207c
Shigeya Suzuki <shigeya@wide.ad.jp>
2010-01-29 13:58:13 +0900

    Initial set of files

---
+++ initial/base-install.sh
@@ -0,0 +1,12 @@
+# Utilities
+yum install -y --enablerepo=rpmforge \
+	zsh nvi
+
+# Programs
+yum install -y --enablerepo=rpmforge \
+	bison elfutils flex gcc-c++ git imake kernel kernel-devel kernel-headers \
+	libX11-devel libXau-devel libXdmcp-devel libevent libevent-devel libgomp \
+	libgssapi libpcap libpcap-devel libselinux-devel libsepol-devel mesa-libGL-devel \
+	nfs-utils-lib nfs-utils-lib-devel pciutils-devel perl perl-Error \
+	perl-Git rcs redhat-rpm-config rpm-build rpmforge-release subversion \
+	subversion-perl tcl tcl-devel tk tk-devel unifdef yum-utils zlib-devel
