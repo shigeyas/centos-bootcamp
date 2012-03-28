ffa9fbaf56506f211cc18556357bbaba32b18408
Shigeya Suzuki <shigeya@wide.ad.jp>
2012-03-28 10:06:24 +0900

    CentOS 6

--- initial/rpmforge-setup.sh
+++ initial/rpmforge-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+case `cat /etc/centos-release` in
+*6.*) R=rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm;;
+*5.*) R=rpmforge-release-0.3.6-1.el5.rf.i386.rpm;;
+*) exit
+esac
 DIR=`dirname $0`
-rpm -Uvh $DIR/../RPMS/rpmforge-release-0.3.6-1.el5.rf.i386.rpm
+rpm -Uvh $DIR/../RPMS/$R
 #sed -i 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/rpmforge.repo
